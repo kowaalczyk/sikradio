d0b403f27fc096cda3aea2dfba4e12eb5577dd54
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-08-12 20:31:55 +0200

    Quick refactor for sender exceptions

--- sender/exceptions.hpp
+++ sender/exceptions.hpp
@@ -5,20 +5,18 @@
 #include <string>
 
 
-namespace sikradio::sender {
-    namespace exceptions {
-        class socket_exception : public std::exception {
-        private:
-            const std::string msg_str;
+namespace sikradio::sender::exceptions {
+    class socket_exception : public std::exception {
+    private:
+        const std::string msg_str;
 
-        public:
-            explicit socket_exception(std::string msg_str) : msg_str(std::move(msg_str)) {}
+    public:
+        explicit socket_exception(std::string msg_str) : msg_str(std::move(msg_str)) {}
 
-            const char *what() const noexcept override {
-                return msg_str.c_str();
-            }
-        };
-    }
+        const char *what() const noexcept override {
+            return msg_str.c_str();
+        }
+    };
 }
 
 
